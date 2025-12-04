b15ff0f1e2c2cf44204ff44fdf1b068d9fc279b7
jairoLAlves <jairoauves8@gmail.com>
2025-12-04 16:00:32 -0300

    Implementando Conta Corrente (Closes #2)

--- src/main/java/br/com/banco/model/ContaCorrente.java
+++ src/main/java/br/com/banco/model/ContaCorrente.java
@@ -1,4 +1,23 @@
 package br.com.banco.model;
 
-public class ContaCorrente {
+public class ContaCorrente extends Conta {
+    private double limiteChequeEspecial;
+
+    public ContaCorrente(String titular, double limiteChequeEspecial) {
+        super(titular);
+        this.limiteChequeEspecial = limiteChequeEspecial;
+    }
+
+    @Override
+    public boolean sacar(double valor) {
+        // Verifica se o valor é coberto pelo saldo + limite
+        if (valor > 0 && (this.saldo + this.limiteChequeEspecial) >= valor) {
+            this.saldo -= valor;
+            System.out.println("Saque de R$ " + valor + " realizado (Conta Corrente) de " + this.titular);
+            return true;
+        } else {
+            System.out.println("Saque insucesso na conta de " + this.titular + " (Saldo + Limite insuficientes)");
+            return false;
+        }
+    }
 }
